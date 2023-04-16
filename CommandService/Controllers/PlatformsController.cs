3a3a049e270ece1e483a3f3e3a19e694dd1cc7e3
Phat Nguyen <phat.nguyen@preferredcomputers.biz>
2023-04-15 23:29:22 -0500

    first commit

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [ApiController]
+    [Route("api/commands/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo repo;
+        private readonly IMapper mapper;
+
+        public PlatformsController(IPlatformRepo repo, IMapper mapper)
+        {
+            this.repo = repo;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var platforms = repo.GetAllPlatforms();
+            var platformsDto = mapper.Map<IEnumerable<PlatformReadDto>>(platforms);
+            return Ok(platformsDto);
+        }
+        [HttpGet("{id}", Name ="GetPlatformById")]
+        public IActionResult GetPlatformById(int id)
+        {
+            var platform = repo.GetPlatformById(id);
+            if(platform != null)
+            {
+                return Ok(mapper.Map<PlatformReadDto>(platform));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Create(PlatformCreateDto platformDto )
+        {
+            var platform = mapper.Map<Platform>(platformDto);
+            repo.CreatePlatform(platform);
+            repo.SaveChanges();
+
+            var platformReadDto = mapper.Map<PlatformReadDto>(platform);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id}, platformReadDto);
+        }
+    }
+}
